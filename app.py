96f7764124faaa59a3af333cb8af81f26fc37985
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-11-11 23:58:07 -0600

    Added description and title.

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from routes.user import user
 
-app = FastAPI()
+app = FastAPI(
+    title="REST API with FastAPI and MongoDB",
+    description="This is a simple REST API using FastAPI and MongoDB",
+    version="1.0.0"
+)
 
 app.include_router(user)
